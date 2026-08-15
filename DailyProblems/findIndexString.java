aec57147d51c51119dbb5b3cc346541a65560d96
Yash <160611725+yashparashar02@users.noreply.github.com>
2026-08-16 00:09:34 +0530

    Solved Index of the 1st Occurence of the Needle in Haystack Problem

--- DailyProblems/findIndexString.java
+++ DailyProblems/findIndexString.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class findIndexString {
   static int findIndex(String haystack, String needle) {
-    int hLen = haystack.length(), nLen = needle.length();
+    int hLen=haystack.length(), nLen=needle.length();
 
-    if (nLen > hLen) 
+    if(nLen > hLen) 
       return -1;
 
-    for (int i=0; i<=hLen-nLen; i++) {
+    for(int i=0; i<=hLen-nLen; i++) {
       int j = 0;
       while(j < nLen && haystack.charAt(i + j) == needle.charAt(j))
         j++;
