702bfe641c19752025b566d3a5b05c27bcd9aa6f
Yash <160611725+yashparashar02@users.noreply.github.com>
2026-08-22 23:59:40 +0530

    Solved

--- DailyProblems/distributeElemArrrays.java
+++ DailyProblems/distributeElemArrrays.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import javax.sound.sampled.SourceDataLine;
+
 public class distributeElemArrrays {
   static int[] resultArray(int[] nums) {
     List<Integer> arr1=new ArrayList<>(), arr2=new ArrayList<>();
@@ -32,6 +34,19 @@ public class distributeElemArrrays {
   public static void main(String[] args) {
     Scanner s=new Scanner(System.in);
 
-    
+    System.out.println("Enter the range of the array");
+    int n=s.nextInt();
+
+    int nums[]=new int[n], i;
+    System.out.println("Enter "+n+" valus of array");
+    for(i=0; i<n; i++)
+      nums[i]=s.nextInt();
+
+    int result[]=resultArray(nums);
+    System.out.print("Result array: ");
+    for(int num : result)
+      System.out.print(num+" ");
+    System.out.println();
+    s.close();
   }
 }
